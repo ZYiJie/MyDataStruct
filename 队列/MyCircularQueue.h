38131252ff7d0d9bd748d616a5944fe16f005617
zhouyijie <921384605@qq.com>
2018-11-06 11:20:03 +0800

    2018.11.6
    增加了循环队列

--- 队列/MyCircularQueue.h
+++ 队列/MyCircularQueue.h
@@ -1,23 +1,27 @@
 #include <iostream>
 #define TRUE 1
-#define QUEUESIZE 100
+#define QUEUESIZE 3
 using namespace std;
+//采用数组队列的模式，在出入队列时使用取余操作实现数组空间的循环使用
+//判断队列的空满有三种操作：1.记录上一次操作2.记录数组使用了的空间3.少用一个空间（我用了第二种方法）
 typedef struct QueueNode {
 	int arr[QUEUESIZE];
 	int begin;
 	int last;
+	int num;
 };
 QueueNode* InitQueue() {
 	QueueNode *top;
 	top = new(QueueNode);
 	top->begin = top->last = 0;
+	top->num = 0;
 	return top;
 }
 int CheckEmpty(QueueNode*top) {
-	return (top->begin==top->last);
+	return (top->num == 0);
 }
 int CheckFull(QueueNode*top) {
-	if (top->last == QUEUESIZE - 1) return 1;
+	if (top->num == QUEUESIZE ) return 1;
 	return 0;
 }
 void EnqueueInput(QueueNode* top) {
@@ -25,6 +29,8 @@ void EnqueueInput(QueueNode* top) {
 	//if (CheckEmpty(top)) top->last++;
 	cin >> top->arr[top->last];
 	top->last++;
+	if (top->last > QUEUESIZE - 1) top->last = (top->last) % QUEUESIZE; //关键：假溢出(即last=3但num<SIZE)时 插入位置为 last%SIZE
+	top->num++;
 	//cout << top->p->data;
 }
 void EnqueueNum(QueueNode* top, int num) {
@@ -32,6 +38,8 @@ void EnqueueNum(QueueNode* top, int num) {
 	//if (CheckEmpty(top)) top->last++;
 	top->arr[top->last] = num;
 	top->last++;
+	if(top->last>QUEUESIZE-1) top->last = (top->last)%QUEUESIZE; //关键：假溢出时 插入位置为 last%SIZE
+	top->num++;
 	//cout << top->p->data;
 }
 int Dequeue(QueueNode*top) {
@@ -39,18 +47,25 @@ int Dequeue(QueueNode*top) {
 	if (CheckEmpty(top)) { cout << "The Queue is empty." << endl; return 0; }
 	result = top->arr[top->begin];
 	top->begin++;
+	top->begin = (top->begin)%QUEUESIZE; //关键：假溢出时 插入位置为 last%SIZE
+	top->num--;
 	//if (top->begin == 1) top->begin--;
 	return result;
 
 }
 void DeleteQueue(QueueNode*top) {
 	top->begin = top->last = 0;
+	top->num = 0;
 	cout << "Delete successfully!";
 	cout << endl;
 }
 
 void TraverseQueue(QueueNode*top) {
-	int i;
-	for (i = top->begin ; i<top->last; i++) cout << top->arr[i] << " ";
+	int i=top->begin,j;
+	for (j = 0; j < top->num;j++) {
+		cout << top->arr[i] << " ";
+		i++;
+		if (i > QUEUESIZE - 1) i = i  % QUEUESIZE; //关键：假溢出时 插入位置为 last%SIZE
+	}
 	cout << endl;
 }
